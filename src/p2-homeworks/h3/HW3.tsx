dbea09065bced0a514ff2db18f248998da49eaff
Marat <marato13@ya.ru>
2022-06-09 14:08:00 +0300

    add homework 3 without CSS styling

--- src/p2-homeworks/h3/HW3.tsx
+++ src/p2-homeworks/h3/HW3.tsx
@@ -1,18 +1,20 @@
 import React, {useState} from 'react'
 import GreetingContainer from './GreetingContainer'
+import {v1} from "uuid";
 
 // types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: string // need to fix any
+    name: string // need to fix any
 }
 
 // уровень работы с глобальными данными
 function HW3() {
-    const [users, setUsers] = useState<any>([]) // need to fix any
+    const [users, setUsers] = useState<Array<UserType>>([]) // need to fix any
 
-    const addUserCallback = (name: any) => { // need to fix any
-        setUsers([]) // need to fix
+    const addUserCallback = (name: string) => { // need to fix any
+        const newUser = {name: name, _id: v1()};
+        setUsers([newUser, ...users]) // need to fix
     }
 
     return (
@@ -21,7 +23,10 @@ function HW3() {
             homeworks 3
 
             {/*should work (должно работать)*/}
-            <GreetingContainer users={users} addUserCallback={addUserCallback}/>
+            <GreetingContainer
+                users={users}
+                addUserCallback={addUserCallback}
+            />
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
