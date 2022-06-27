236a1c52dcc4f621bc4b111ce0e4dd25c6018764
Marat <marato13@ya.ru>
2022-06-27 11:43:01 +0300

    add 5 homework change master to main branch

--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import Header from './Header'
 import Routes from './Routes'
+import Pages from "../h5-rrd-v6/Pages";
+import {HashRouter} from "react-router-dom";
 
 function HW5() {
     return (
         <div>
             {/*в gh-pages лучше работает HashRouter*/}
-            {/*<HashRouter>*/}
+            <HashRouter>
 
             <Header/>
 
-            <Routes/>
+            <Pages/>
 
-            {/*</HashRouter>*/}
+            </HashRouter>
         </div>
     )
 }
