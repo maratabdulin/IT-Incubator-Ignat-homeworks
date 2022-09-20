b0c67355ebfd29d717625ba68111b4de2b69f54d
Marat <marato13@ya.ru>
2022-09-20 19:46:28 +0300

    add emotion css in js from HW8

--- src/p2-homeworks/h7/HW7.tsx
+++ src/p2-homeworks/h7/HW7.tsx
@@ -4,10 +4,22 @@ import React, {useState} from 'react'
 import SuperSelect from './common/c5-SuperSelect/SuperSelect'
 import SuperRadio from './common/c6-SuperRadio/SuperRadio'
 import Title from "../components/title/Title";
-import s from './hw7.module.css'
 import sectionWrapper from '../assets/styles/sectionWrapper';
+import {css} from '@emotion/react';
 
 const arr = ['x', 'y', 'z']
+
+const superSelect = css`
+  margin: 0 auto 30px;
+  padding: 10px;
+`
+
+const superRadio = css`
+  display: flex;
+  gap: 30px;
+  margin: 0 auto 30px;
+  padding: 10px;
+`
 
 function HW7() {
     const [value, onChangeOption] = useState(arr[1])
@@ -15,15 +27,14 @@ function HW7() {
     return (
         <div css={sectionWrapper}>
             <Title text={'homeworks 7'} titleLevel={2}/>
-            {/*should work (должно работать)*/}
-            <div className={s.superSelect}>
+            <div css={superSelect}>
                 <SuperSelect
                     options={arr}
                     value={value}
                     onChangeOption={onChangeOption}
                 />
             </div>
-            <div className={s.superRadio}>
+            <div css={superRadio}>
                 <SuperRadio
                     name={'radio'}
                     options={arr}
--- src/p2-homeworks/h8/HW8.tsx
+++ src/p2-homeworks/h8/HW8.tsx
@@ -3,10 +3,15 @@
 import React, {useState} from 'react'
 import {checkAC, homeWorkReducer, sortAC} from './bll/homeWorkReducer'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
-import Title from "../components/title/Title";
-import style from './HW8.module.css'
+import Title from '../components/title/Title';
+import sectionWrapper from '../assets/styles/sectionWrapper';
+import {css} from '@emotion/react';
+import {useSelector} from 'react-redux';
+import {AppStoreType} from '../h10/bll/store';
+import {ThemeTypes} from '../h10/bll/setThemeReducer';
+import colors from '../assets/styles/colors';
 
-export type UserType ={
+export type UserType = {
     _id: number
     name: string
     age: number
@@ -21,12 +26,51 @@ const initialPeople: Array<UserType> = [
     {_id: 5, name: 'Irina', age: 55},
 ]
 
+const buttonWrapper = css`
+  display: flex;
+  gap: 30px;
+  justify-content: start;
+  width: 50%;
+  margin: 0 auto;
+`
+
+const userInfo = css`
+  display: flex;
+  justify-content: space-between;
+  font-family: "Fredoka One", cursive;
+  font-size: 20px;
+  font-weight: 700;
+  color: white;
+`
+
+const userWrapper = css`
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+  box-sizing: border-box;
+  width: 50%;
+  margin: 20px auto 0;
+  padding: 20px;
+  border-radius: 5px;
+  transition: .5s;
+`
+
 function HW8() {
     const [people, setPeople] = useState<Array<UserType>>(initialPeople) // need to fix any
 
+    let themeFromState = useSelector<AppStoreType, ThemeTypes>(state => state.theme.theme);
+
+    const userInfoTheme = css`
+      color: ${colors[themeFromState].main}
+    `
+
+    const userWrapperTheme = css`
+      background-color: ${colors[themeFromState].bubbleBackground};
+      filter: drop-shadow(0 0 15px ${colors[themeFromState].main}) drop-shadow(0 0 50px ${colors[themeFromState].main}) contrast(2) brightness(2);
+    `
     // need to fix any
     const finalPeople = people.map((p: UserType) => (
-        <div key={p._id} className={style.userInfo}>
+        <div key={p._id} css={[userInfo, userInfoTheme]}>
             <div>{p.name}</div>
             <div>{p.age}</div>
         </div>
@@ -37,13 +81,13 @@ function HW8() {
     const check18 = () => setPeople(homeWorkReducer(initialPeople, checkAC(18)))
 
     return (
-        <div className={style.container}>
+        <div css={sectionWrapper}>
             <Title text={'homeworks 8'} titleLevel={2}/>
             {/*should work (должно работать)*/}
-            <div className={style.usersWrapper}>
+            <div css={[userWrapper, userWrapperTheme]}>
                 {finalPeople}
             </div>
-            <div className={style.buttonsWrapper}>
+            <div css={buttonWrapper}>
                 <SuperButton onClick={sortUp}>sort up</SuperButton>
                 <SuperButton onClick={sortDown}>sort down</SuperButton>
                 <SuperButton onClick={check18}>check 18</SuperButton>
